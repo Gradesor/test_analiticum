0cf45efcd734b1f63867ba9da03e1f38b40dbb6f
Gradesor <heruvim694@gmail.com>
2019-08-30 23:30:37 +0500

    Таки понял как это делается, я бы конечно сдался, и сказал что не смогу сделать то что меня просят, если бы это не было моей давней целью, так что свёрстана ещё одна секция но времени ушло уйма что бы прийти к display: table, отчитываюсь тут будто это кто-то читает, что вроде блога уже получается:D

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
 		$('.nav-burg__menu').toggleClass('nav-burg__menu_active');
 		$('#menu1').removeClass('nav-popup_active');
 	})
+	// слайдер с кухнями
+	var mySwiper = new Swiper('.product-slider', {
+		loop: true,
+		loopFillGroupWithBlank: true,
+		slidesPerView: 'auto',
+		slidesPerGroup: 1,
+		spaceBetween: 0,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.product-slider__prev',
+			prevEl: '.product-slider__next',
+		},
+	})
 })
