22bd917f609f426d5677326f791b7d49a7f0fdb4
Gradesor <heruvim694@gmail.com>
2019-08-31 06:15:52 +0500

    свёрстана секция promotions

--- src/js/main.js
+++ src/js/main.js
@@ -12,10 +12,37 @@ $(document).ready(function () {
 		slidesPerView: 'auto',
 		slidesPerGroup: 1,
 		spaceBetween: 0,
+		nested:true,
 		centeredSlides: true,
 		navigation: {
 			nextEl: '.product-slider__prev',
 			prevEl: '.product-slider__next',
 		},
+	})
+	// слайдер с акциями
+	var mySwipe2 = new Swiper('.promotions-slider', {
+		loop: true,
+		loopFillGroupWithBlank: true,
+		slidesPerView: 2,
+		slidesPerGroup: 1,
+		spaceBetween: 30,
+		nested:true,
+		pagination: {
+		el: '.promotions-slider__pagination',
+		},
+		breakpoints: {			
+			1060: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			768: {
+				slidesPerView: 2
+			},
+			576: {
+				slidesPerView: 1
+
+			}
+		}
+
 	})
 })
